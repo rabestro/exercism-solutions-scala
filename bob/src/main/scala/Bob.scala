172e52a35377c7ffa9d24868d78f25b131f9a73e
Jegors Cemisovs <jcemisovs@evolution.com>
2025-01-20 10:56:19 +0200

    add Bob exercise

---
+++ bob/src/main/scala/Bob.scala
@@ -0,0 +1,17 @@
+object Bob {
+  private val isYelling = "[^a-z]*[A-Z][^a-z]*".r
+  private def isQuestion: String => Boolean = _.endsWith("?")
+
+  def response(statement: String): String = statement.trim match {
+    case s @ isYelling() if isQuestion(s) =>
+      "Calm down, I know what I'm doing!"
+    case isYelling() =>
+      "Whoa, chill out!"
+    case s if isQuestion(s) =>
+      "Sure."
+    case "" =>
+      "Fine. Be that way!"
+    case _ =>
+      "Whatever."
+  }
+}
