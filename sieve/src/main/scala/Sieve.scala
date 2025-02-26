c7ee6d99912460ea10e317cd3c8a42e5f3592eb7
Jegors Cemisovs <jcemisovs@evolution.com>
2025-02-26 16:15:47 +0200

    Update prime generation algorithm in Sieve.scala
    Improve performance by using a BitSet to track unmarked numbers and optimize the loop for marking multiples.

--- sieve/src/main/scala/Sieve.scala
+++ sieve/src/main/scala/Sieve.scala
@@ -2,9 +2,11 @@ import scala.collection.mutable
 
 object Sieve:
   def primes(number: Int): List[Int] =
-    val numbers = mutable.BitSet().addAll(2 to number)
+    val unmarkedNumbers = mutable.BitSet(2 to number *)
+    val limit = math.sqrt(number).toInt
 
+    for (x <- 2 to limit if unmarkedNumbers contains x) do
+      for (multiple <- x * x to number by x) do
+        unmarkedNumbers -= multiple
 
-    numbers.toList
-
-          
+    unmarkedNumbers.toList      
