722f241f4356de6ddaa8e8c44e4eb95ea68455ad
Jegors Cemisovs <jcemisovs@evolution.com>
2025-02-28 11:04:27 +0200

    Update Sieve.scala: Simplify prime generation loop
    This commit simplifies the prime generation loop in the Sieve.scala file by removing the explicit limit calculation and iterating over the unmarked numbers directly. This change should improve the performance of the Sieve of Eratosthenes algorithm.

--- sieve/src/main/scala/Sieve.scala
+++ sieve/src/main/scala/Sieve.scala
@@ -3,9 +3,8 @@ import scala.collection.mutable
 object Sieve:
   def primes(number: Int): List[Int] =
     val unmarkedNumbers = mutable.BitSet(2 to number *)
-    val limit = math.sqrt(number).toInt
 
-    for (x <- 2 to limit if unmarkedNumbers contains x) do
+    for (x <- unmarkedNumbers) do
       for (multiple <- x * x to number by x) do
         unmarkedNumbers -= multiple
 
