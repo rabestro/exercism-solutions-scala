484f4e306004ac6b41135d2ac3af317d3d2dbaa4
Jegors Cemisovs <jcemisovs@evolution.com>
2025-02-28 11:06:59 +0200

    refactor parameter name

--- sieve/src/main/scala/Sieve.scala
+++ sieve/src/main/scala/Sieve.scala
@@ -1,11 +1,11 @@
 import scala.collection.mutable
 
 object Sieve:
-  def primes(number: Int): List[Int] =
-    val unmarkedNumbers = mutable.BitSet(2 to number *)
+  def primes(limit: Int): List[Int] =
+    val unmarkedNumbers = mutable.BitSet(2 to limit *)
 
     for (x <- unmarkedNumbers) do
-      for (multiple <- x * x to number by x) do
+      for (multiple <- x * x to limit by x) do
         unmarkedNumbers -= multiple
 
     unmarkedNumbers.toList      
