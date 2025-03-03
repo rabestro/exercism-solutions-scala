9ce51aa1751583dcf70a856e5093c933ecc9da91
Jegors Cemisovs <jcemisovs@evolution.com>
2025-03-03 20:13:08 +0200

    Update Scala version and fix pending tests in ClockTest.scala
    Upgraded Scala version to 3.6.3 in build.sbt and fixed all pending tests in ClockTest.scala.

--- clock/src/test/scala/ClockTest.scala
+++ clock/src/test/scala/ClockTest.scala
@@ -118,112 +118,90 @@ class ClockTest extends AnyFunSuite with Matchers {
   }
 
   test("subtract to previous hour") {
-    pending
     Clock(10, 3) - Clock(30) should be(Clock(9, 33))
   }
 
   test("subtract more than an hour") {
-    pending
     Clock(10, 3) - Clock(70) should be(Clock(8, 53))
   }
 
   test("subtract across midnight") {
-    pending
     Clock(0, 3) - Clock(4) should be(Clock(23, 59))
   }
 
   test("subtract more than two hours") {
-    pending
     Clock(0, 0) - Clock(160) should be(Clock(21, 20))
   }
 
   test("subtract more than two hours with borrow") {
-    pending
     Clock(6, 15) - Clock(160) should be(Clock(3, 35))
   }
 
   test("subtract more than one day (1500 min = 25 hrs)") {
-    pending
     Clock(5, 32) - Clock(1500) should be(Clock(4, 32))
   }
 
   test("subtract more than two days") {
-    pending
     Clock(2, 20) - Clock(3000) should be(Clock(0, 20))
   }
 
   test("clocks with same time") {
-    pending
     Clock(15, 37) == Clock(15, 37) should be(true)
   }
 
   test("clocks a minute apart") {
-    pending
     Clock(15, 36) == Clock(15, 37) should be(false)
   }
 
   test("clocks an hour apart") {
-    pending
     Clock(14, 37) == Clock(15, 37) should be(false)
   }
 
   test("clocks with hour overflow") {
-    pending
     Clock(10, 37) == Clock(34, 37) should be(true)
   }
 
   test("clocks with hour overflow by several days") {
-    pending
     Clock(3, 11) == Clock(99, 11) should be(true)
   }
 
   test("clocks with negative hour") {
-    pending
     Clock(22, 40) == Clock(-2, 40) should be(true)
   }
 
   test("clocks with negative hour that wraps") {
-    pending
     Clock(17, 3) == Clock(-31, 3) should be(true)
   }
 
   test("clocks with negative hour that wraps multiple times") {
-    pending
     Clock(13, 49) == Clock(-83, 49) should be(true)
   }
 
   test("clocks with minute overflow") {
-    pending
     Clock(0, 1) == Clock(0, 1441) should be(true)
   }
 
   test("clocks with minute overflow by several days") {
-    pending
     Clock(2, 2) == Clock(2, 4322) should be(true)
   }
 
   test("clocks with negative minute") {
-    pending
     Clock(2, 40) == Clock(3, -20) should be(true)
   }
 
   test("clocks with negative minute that wraps") {
-    pending
     Clock(4, 10) == Clock(5, -1490) should be(true)
   }
 
   test("clocks with negative minute that wraps multiple times") {
-    pending
     Clock(6, 15) == Clock(6, -4305) should be(true)
   }
 
   test("clocks with negative hours and minutes") {
-    pending
     Clock(7, 32) == Clock(-12, -268) should be(true)
   }
 
   test("clocks with negative hours and minutes that wrap") {
-    pending
     Clock(18, 7) == Clock(-54, -11513) should be(true)
   }
 }
