3cc6438663052709eb325b1406af7f2a0620f8e2
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-12 14:37:11 +0300

    add SpiralMatrix solution

--- spiral-matrix/src/main/scala/SpiralMatrix.scala
+++ spiral-matrix/src/main/scala/SpiralMatrix.scala
@@ -1,2 +1,15 @@
 object SpiralMatrix:
-  def spiralMatrix(size: Int): List[List[Int]] = List()
+  def spiralMatrix(size: Int, start: Int = 1): List[List[Int]] = size match
+    case 0 => Nil
+    case 1 => List(List(start))
+    case _ =>
+      val step = size - 1
+      val inner = spiralMatrix(size - 2, start + step * 4)
+      val middle = 1 until step map { i =>
+        val left = start + step * 4 - i
+        val right = start + step + i
+        left :: inner(i - 1) ::: right :: Nil
+      }
+      val top = start to start + step
+      val bottom = start + step * 3 to start + step * 2 by -1
+      top.toList :: middle.toList ::: bottom.toList :: Nil
