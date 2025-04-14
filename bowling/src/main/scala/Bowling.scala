c9a6272bde081f74eae1562fab8255121e800385
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-14 15:18:37 +0300

    update implementation

--- bowling/src/main/scala/Bowling.scala
+++ bowling/src/main/scala/Bowling.scala
@@ -1,68 +1,72 @@
 sealed trait Bowling:
+  val MaxFrames = 10
+  val MinPins = 0
+  val MaxPins = 10
+
   def roll(pins: Int): Bowling
 
   def score(): Either[String, Int]
 
 object Bowling:
-  def apply(): Bowling = RunningGame(Nil, None)
+  def apply(): Bowling = RunningGame(Nil, EmptyFrame)
+
+case class RunningGame(completed: List[CompletedFrame], current: StateFrame) extends Bowling:
+
+  override def roll(pins: Int): Bowling =
+    if pins < MinPins || pins > MaxPins then
+      IncorrectGame("Pins must have a value from 0 to 10")
+    else current match
+      case IncompleteFrame(first) if first + pins > MaxPins =>
+        IncorrectGame("Pin count exceeds pins on the lane")
+
+      case IncompleteFrame(first) if first + pins == MaxPins =>
+        RunningGame(SpareFrame(first, pins) :: completed, EmptyFrame)
+
+      case IncompleteFrame(first) =>
+        val frames = OpenFrame(first, pins) :: completed
+        frames.length match
+          case MaxFrames => CompletedGame(frames.reverse)
+          case _ => RunningGame(frames, EmptyFrame)
+
+      case FillBallsFrame(balls) =>
+        CompletedGame(completed.reverse, balls :+ pins)
+
+      case EmptyFrame if completed.length == MaxFrames =>
+        val fillBallsFrame = FillBallsFrame(List(pins))
+        if completed.head.isInstanceOf[StrikeFrame] then
+          RunningGame(completed, fillBallsFrame)
+        else
+          CompletedGame(completed.reverse, pins :: Nil)
+
+      case EmptyFrame if pins == 10 =>
+        RunningGame(StrikeFrame() :: completed, EmptyFrame)
+
+      case EmptyFrame =>
+        RunningGame(completed, IncompleteFrame(pins))
+
+
+  override def score(): Either[String, Int] =
+    Left("Score cannot be taken until the end of the game")
 
 case class IncorrectGame(reason: String) extends Bowling:
   override def roll(pins: Int): Bowling = this
 
   override def score(): Either[String, Int] = Left(reason)
 
-case class RunningGame(completed: List[Frame], current: Option[Frame]) extends Bowling:
-  private def isComplete: Boolean = completed.length == 10
-    && (completed.head.isInstanceOf[OpenFrame]
-    || current.isDefined && current.get.isInstanceOf[FillBallsFrame] && (
-    completed.head.isInstanceOf[SpareFrame]
-      && current.get.asInstanceOf[FillBallsFrame].rolls.length == 1
-      || completed.head.isInstanceOf[StrikeFrame]
-      && current.get.asInstanceOf[FillBallsFrame].rolls.length == 2
-    )
-    )
-
+case class CompletedGame(frames: List[Frame], fillBalls: List[Int] = Nil) extends Bowling:
   override def roll(pins: Int): Bowling =
-    current match
-      case _ if isComplete =>
-        IncorrectGame("Should not be able to roll after game is over")
+    IncorrectGame("Should not be able to roll after game is over")
 
-      case _ if pins < 0 || pins > 10 =>
-        IncorrectGame("Pins must have a value from 0 to 10")
+  private def nextRolls(tail: List[CompletedFrame]): LazyList[Int] =
+    LazyList.from(tail.flatMap(_.rolls) ::: fillBalls)
 
-      case Some(IncompleteFrame(first)) if first + pins > 10 =>
-        IncorrectGame("Pin count exceeds pins on the lane")
+  private def scoreFrame(frame: CompletedFrame, nextFrames: List[CompletedFrame]): Int =
+    frame match
+      case _:StrikeFrame => 10 + nextRolls(nextFrames).take(2).sum
+      case SpareFrame(first, second) => first + second + nextRolls(nextFrames).head
+      case OpenFrame(first, second) => first + second
 
-      case Some(FillBallsFrame(List(first))) if first + pins > 10 =>
-        IncorrectGame("Pin count exceeds pins on the lane")
 
-      case Some(IncompleteFrame(first)) if first + pins == 10 =>
-        RunningGame(SpareFrame(first, pins) :: completed, None)
 
-      case Some(IncompleteFrame(first)) =>
-        RunningGame(OpenFrame(first, pins) :: completed, None)
-
-      case Some(FillBallsFrame(balls)) =>
-        RunningGame(completed, Some(FillBallsFrame(pins :: balls)))
-
-      case None if completed.length == 10 =>
-        RunningGame(completed, Some(FillBallsFrame(List(pins))))
-
-      case None if completed.length == 10 =>
-        RunningGame(completed, Some(IncompleteFrame(pins)))
-
-      case None if pins == 10 =>
-        RunningGame(StrikeFrame() :: completed, None)
-
-      case None =>
-        RunningGame(completed, Some(IncompleteFrame(pins)))
-
-      case Some(_) =>
-        IncorrectGame("Game should not be in this state")
-
-  override def score(): Either[String, Int] =
-    if !isComplete then
-      Left("Score cannot be taken until the end of the game")
-    else
-      Right(0)
+  override def score(): Either[String, Int] = Right(0)
 
--- bowling/src/main/scala/Frame.scala
+++ bowling/src/main/scala/Frame.scala
@@ -1,16 +1,21 @@
-sealed trait Frame:
+sealed trait Frame
+
+sealed trait CompletedFrame extends Frame:
   def rolls: List[Int]
 
-case class IncompleteFrame(first: Int) extends Frame:
-  def rolls: List[Int] = List(first)
-
-case class OpenFrame(first: Int, second: Int) extends Frame:
+case class OpenFrame(first: Int, second: Int) extends CompletedFrame:
   def rolls: List[Int] = List(first, second)
 
-case class SpareFrame(first: Int, second: Int) extends Frame:
+case class SpareFrame(first: Int, second: Int) extends CompletedFrame:
   def rolls: List[Int] = List(first, second)
 
-case class StrikeFrame() extends Frame:
+case class StrikeFrame() extends CompletedFrame:
   def rolls: List[Int] = List(10)
 
-case class FillBallsFrame(rolls: List[Int]) extends Frame
+sealed trait StateFrame extends Frame
+
+case object EmptyFrame extends StateFrame
+
+case class IncompleteFrame(first: Int) extends StateFrame
+
+case class FillBallsFrame(rolls: List[Int]) extends StateFrame
