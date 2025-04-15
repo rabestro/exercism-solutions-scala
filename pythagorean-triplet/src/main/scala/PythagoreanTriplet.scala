2b330acf5ed4e86b2e1e11f0ac79cd23af436dfe
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-15 15:07:46 +0300

    add implementation for pythagoreanTriplets

--- pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
+++ pythagorean-triplet/src/main/scala/PythagoreanTriplet.scala
@@ -3,7 +3,14 @@ object PythagoreanTriplet:
     val (a, b, c) = candidate
     a < b && b < c && a * a + b * b == c * c
 
-  def pythagoreanTriplets(from: Int, to: Int): Seq[(Int, Int, Int)] = Nil
+  def pythagoreanTriplets(min: Int, max: Int): Seq[(Int, Int, Int)] =
+    for {
+      a <- min to max - 2
+      b <- a until max
+      c <- b to max
+      candidate  = (a, b, c)
+      if isPythagorean(candidate)
+    } yield candidate
 
   def pythagoreanTripletsSum(sum: Int): Seq[(Int, Int, Int)] = Nil
 
