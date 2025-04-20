54d4ce3afc0e39ebbdd5e00099746c4e522411fa
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-20 21:55:21 +0300

    Refactor coin change algorithm with optimal approach
    Replace the greedy algorithm with a dynamic programming-based approach to find the fewest coins needed for a given amount. Leverage mutable collections for efficient computation and extend functionality to handle more complex cases. Ensure backward compatibility by retaining the previous greedy method as a private fallback.

--- change/src/main/scala/Change.scala
+++ change/src/main/scala/Change.scala
@@ -1,13 +1,31 @@
-import scala.collection.SortedSet
+import scala.collection.{SortedSet, mutable}
 
 object Change:
   def findFewestCoins(amount: Int, denominations: List[Int]): Option[List[Int]] =
-    greedy(amount, SortedSet(denominations *))
+    optimal(amount, SortedSet(denominations *))
 
+  private def optimal(amount: Int, denominations: SortedSet[Int]): Option[List[Int]] =
+    val changes = mutable.HashMap[Int, List[Int]](0 -> Nil)
+    val amounts = mutable.ArrayDeque[Int](0)
 
-  def greedy(amount: Int, denominations: SortedSet[Int]): Option[List[Int]] =
+    while (amounts.nonEmpty)
+      val head = amounts.removeHead()
+      val value = changes.get(head)
+      if head == amount then
+        return value.map(_.reverse)
+
+      denominations.foreach { coin =>
+        val total = head + coin
+        if total <= amount && !changes.contains(total) then
+          amounts addOne total
+          val change = coin :: value.getOrElse(Nil)
+          changes.put(total, change)
+      }
+    None
+
+  private def greedy(amount: Int, denominations: SortedSet[Int]): Option[List[Int]] =
     def change(amount: Int, result: List[Int]): Option[List[Int]] =
-      if amount <= 0 then Option(result)
+      if amount == 0 then Option(result)
       else denominations.maxBefore(amount + 1)
         .flatMap(coin => change(amount - coin, coin :: result))
 
