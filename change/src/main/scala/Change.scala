100960c1245088d1e219bc33753b5be072d3cfc9
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-20 19:53:43 +0300

    Add greedy algorithm for coin change problem
    Implement a greedy algorithm to find the fewest coins for making change given an amount and coin denominations. Update test cases to validate the implementation, replacing `pending` placeholders with actual assertions. Include the use of `SortedSet` for efficient denomination handling.

--- change/src/main/scala/Change.scala
+++ change/src/main/scala/Change.scala
@@ -1,3 +1,14 @@
+import scala.collection.SortedSet
+
 object Change:
-  def findFewestCoins(amount: Int, denominations: List[Int]): Option[List[Int]] = 
-    Some(List(25))
+  def findFewestCoins(amount: Int, denominations: List[Int]): Option[List[Int]] =
+    greedy(amount, SortedSet(denominations *))
+
+
+  def greedy(amount: Int, denominations: SortedSet[Int]): Option[List[Int]] =
+    def change(amount: Int, result: List[Int]): Option[List[Int]] =
+      if amount <= 0 then Option(result)
+      else denominations.maxBefore(amount + 1)
+        .flatMap(coin => change(amount - coin, coin :: result))
+
+    change(amount, Nil)
