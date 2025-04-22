5a881fdecf2eb11c99d61cb52ad224ea7af78696
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-22 15:05:04 +0300

    Implement `Cipher` class with random key generation and encoding
    Introduces a `Cipher` class that supports encoding using a random key or a provided key. The implementation generates random keys with a configurable length and encodes text by combining the input with a keystream. This update also includes utility methods for key creation and character encoding.

--- simple-cipher/src/main/scala/Cipher.scala
+++ simple-cipher/src/main/scala/Cipher.scala
@@ -1,5 +1,31 @@
+import Cipher.randomKey
+
+import scala.util.Random
+
 class Cipher(cipherKey: Option[String]):
-  val key: String = cipherKey.getOrElse("aaaaaaaaaa")
-  def encode(text: String): String = "aaaaaaaaaa"
+  val key: String = cipherKey getOrElse randomKey
+  val keyStream: Seq[Char] = LazyList.continually(key).flatten
+
+  def encode(text: String): String =
+    text.to(LazyList).zip(keyStream).map(Cipher.encode).mkString
+
   def decode(key: String): String = "aaaaaaaaaa"
+
+
+object Cipher:
+  private val MinKeyLength = 100
+  private val MaxKeyLength = 1000000
+  private val lettersInAlphabet = 'z' - 'a' + 1
+
+  def randomKey: String =
+    val keyLength = Random.between(MinKeyLength, MaxKeyLength)
+    Random.alphanumeric.filter(_.isLower).take(keyLength).mkString
+
+  def encode(symbol: Char, key: Char): Char =
+    val keyIndex = key - 'a'
+    val symbolIndex = symbol - 'a'
+    val encodedSymbol = (symbolIndex + keyIndex) % lettersInAlphabet + 'a'
+    encodedSymbol.toChar
+
+  
   
