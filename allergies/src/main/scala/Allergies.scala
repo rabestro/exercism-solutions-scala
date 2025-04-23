de736c551fe8eca290a145adf1013f6faea00e60
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-23 21:43:40 +0300

    Add Allergies exercise implementation with tests
    Implemented functionality to determine allergies based on a score, including methods to check individual allergens and list all allergens. Added comprehensive tests and initial project setup files for the Scala environment.

---
+++ allergies/src/main/scala/Allergies.scala
@@ -0,0 +1,8 @@
+enum Allergen:
+  case Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats
+
+object Allergies:
+  def allergicTo(allergen: Allergen, allergies: Int): Boolean =
+    ((1 << allergen.ordinal) & allergies) != 0
+  
+  def list(allergies: Int): List[Allergen] = List()
