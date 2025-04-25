1f60964d0083dc207301dafb90114be3ed9e4a60
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-25 17:16:56 +0300

    Add initial implementation of ComplexNumber class and tests
    Introduces a `ComplexNumber` class with basic operations like addition, subtraction, multiplication, division, conjugate, and absolute value. Includes a comprehensive test suite using ScalaTest and supporting project configuration files (`build.sbt`, `build.properties`, and `README.md`).

---
+++ complex-numbers/src/main/scala/ComplexNumber.scala
@@ -0,0 +1,13 @@
+final case class ComplexNumber(real: Double = 0, imaginary: Double = 0):
+  def +(that: ComplexNumber): ComplexNumber = 
+    ComplexNumber(real + that.real, imaginary + that.imaginary)
+  def -(that: ComplexNumber): ComplexNumber = ???
+  def *(that: ComplexNumber): ComplexNumber = ???
+  def /(that: ComplexNumber): ComplexNumber = ???
+  def abs: Double = ???
+  def conjugate: ComplexNumber = ???
+  
+  
+object ComplexNumber:
+  def exp(z: ComplexNumber): ComplexNumber = ???  
+  
